a5e37b047cba4f2ca356cc4892e275cf420fa9df
Benjamin Cubillos <bcubillos@usm.cl>
2023-10-18 21:42:41 -0300

    01723087

--- projecto_intro/contaminacion/apps.py
+++ projecto_intro/contaminacion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ContaminacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contaminacion'
+    name = 'contaminacion
